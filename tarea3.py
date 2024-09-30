bf6250c2ea986791e54f8158079aef05ba02ad0c
BaltyyHyype <l22120710@morelia.tecnm.mx>
2024-09-30 13:44:10 -0600

    Añadiendo archivos nuevos

--- tarea3.py
+++ tarea3.py
@@ -1,31 +1,31 @@
 ##############################################
-# actv 1 rotar=60, trasladar=10px, escala=1/5
+# actv 1 rotar 70 grados, escalar a 2, trasladar 20px
 ##############################################
 
 import cv2 as cv
 import numpy as np
 import math
 
-# carquekos la imagen en escala de grises
+# cargar la imagen en escala de grises
 img = cv.imread('chevrolet.png', 0)
 
-# para que obtengamos el amaño de la imagen
+# obtener el tamaño de la imagen
 x, y = img.shape
 
-# convertirlo a radianes lpos grados de la imagej
-angle = 60
+# definir el angulo de rotacion
+angle = 70  # rotar 70 grados
 theta = math.radians(angle)
 
-# creanios una imagen vacía para poder almacenar la imagen ya rotada
+# crear una imagen vacia para la imagen rotada
 rotated_img = np.zeros((x * 2, y * 2), dtype=np.uint8)
 xx, yy = rotated_img.shape
 
-# cceeentro de la imagen
+# calcular el centro de la imagen
 cx, cy = int(x // 2), int(y // 2)
 
 ##############################################################################
 
-# rotamos la imagen
+# rotar la imagen 70 grados a la derecha
 for i in range(x):
     for j in range(y):
         new_x = int((j - cx) * math.cos(theta) - (i - cy) * math.sin(theta) + cx)
@@ -35,28 +35,29 @@ for i in range(x):
 
 ######################################################################
 
-# escala de 1/5 de act1
-scale_x, scale_y = 1/5, 1/5
+# definir el factor de escala
+scale_x, scale_y = 2, 2  # escalar a 2
 
-# creamos nueva imagen para almacenar el escalado
+# crear una nueva imagen para almacenar el escalado
 scaled_img = np.zeros((int(x * scale_y), int(y * scale_x)), dtype=np.uint8)
 
-#################################################################333
-#eeescalado
+######################################################################
+
+# aplicar el escalado
 for i in range(int(x * scale_y)):
     for j in range(int(y * scale_x)):
         orig_x = int(i / scale_y)
         orig_y = int(j / scale_x)
         if 0 <= orig_x < xx and 0 <= orig_y < yy:
             scaled_img[i, j] = rotated_img[orig_x, orig_y]
+
 ########################################################################
 
-
-# imagen vacía para la traslación
+# crear una imagen vacia para la traslacion
 final_img = np.zeros((scaled_img.shape[0], scaled_img.shape[1]), dtype=np.uint8)
 
-# desplazamiento en el ekje x y en el eje y
-dx, dy = 10, 10
+# desplazamiento en el eje x y en el eje y
+dx, dy = 20, 20  # trasladar 20 pixeles
 
 # trasladar la imagen escalada
 for i in range(scaled_img.shape[0]):
@@ -66,10 +67,12 @@ for i in range(scaled_img.shape[0]):
         if 0 <= new_x < final_img.shape[0] and 0 <= new_y < final_img.shape[1]:
             final_img[new_x, new_y] = scaled_img[i, j]
 
-# imprimimos la imagen original, rotada, escalada y trasladada
-cv.imshow('Imagen Original', img)
-cv.imshow('Imagen Rotada 60 grados', rotated_img)
-cv.imshow('Imagen Escalada 1/5', scaled_img)
-cv.imshow('Imagen Final Trasladada', final_img)
+########################################################################
+
+# mostrar las imagenes
+cv.imshow('imagen original', img)
+cv.imshow('imagen rotada 70 grados', rotated_img)
+cv.imshow('imagen escalada a 2', scaled_img)
+cv.imshow('imagen final trasladada', final_img)
 cv.waitKey(0)
 cv.destroyAllWindows()
